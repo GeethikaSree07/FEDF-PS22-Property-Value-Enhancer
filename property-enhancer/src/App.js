38c9ecdbae625b1ca5b7a2bdd920b43c359d03f7
2400080007 <2400080007@kluniversity.in>
2025-11-03 11:44:48 +0530

    Add files via upload

---
+++ property-enhancer/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+function App(){
+  return (
+    <div className="card">
+      <h2>Property Enhancer</h2>
+      <p>App Working ✅</p>
+    </div>
+  );
+}
+export default App;
